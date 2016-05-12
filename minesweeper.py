90386922aa74f8bc4371ea048537af6f524684f9
somyajain <somyajain@gmail.com>
2016-05-12 22:33:30 +0530

    mine positions

--- minesweeper.py
+++ minesweeper.py
@@ -3,26 +3,52 @@
 
 from Tkinter import *
 import tkMessageBox
-
+from random import *
+import math
 frame = Tk()
 
 
-Label(frame, text="Enter the grid size n:").grid(row=0)
+Label(frame, text="Enter the grid size n:").grid(row=0,column=0)
 e1=Entry(frame)
-e1.grid(row=0,column=1)
+e1.grid(row=1,column=0)
+
+def mine_positions(n,m):
+    no_of_mines=math.floor((n**2)/4.0)
+    print 'no_of_mines',no_of_mines
+    pos=[]
+    for i in range(0,int(no_of_mines)):
+        while True:
+            k=randint(0,len(m))
+            print len(m)
+            z=(m)[k]
+            if z not in pos:
+                pos.append(z)
+                break
+    return pos
+
 
 def fetch_grid_size():
     grid_size = e1.get()
-    type(grid_size)
-
-
+    size_of_grid=int(grid_size)
     x={}
-    for i in range (1,grid_size+1):
-        for j in range(1,grid_size+1):
+    for i in range (1,size_of_grid+1):
+        for j in range(1,size_of_grid+1):
+            x[i,j]=Button(frame,text=str(i)+','+str(j),height=1,width=1)
+            x[i,j].grid(row=i,column=j)
 
-            x[i,j]=Button(frame,text=str(i)+str(j),height=2,width=2).grid(row=i,column=j)
+        (x.keys()).sort()
 
-Button(frame,text='ok',command=fetch_grid_size).grid(row=1,column=0,sticky=W,pady=4)
+    mine_pos=mine_positions(size_of_grid,x.keys())
+    print mine_pos
+
+
+
+
+Button(frame,text='ok',command=fetch_grid_size).grid(row=2,column=0,sticky=W,pady=4)
+
+
+
+
 
 
 
